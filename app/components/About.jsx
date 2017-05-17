611a6c276407e25c7d7c0c6eb61774540a05e869
Joshua Anthony <jantho1990@gmail.com>
2017-05-17 00:23:35 -0500

    added app.css and added custom styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var About = (props) => (
   <div>
-    <h1 className="text-center">About</h1>
+    <h1 className="text-center page-title">About</h1>
     <p>This is a simple weather application built by Josh Anthony for the Complete React Web App Developer course on Udemy. To use, go to the Get Weather page and type a city name and state into the search bar. You can also use the search bar at the top of the page.</p>
     <br/>
     <p>Built with <a href="https://facebook.github.io/react/">ReactJS</a> and <a href="http://openweathermap.org/">Open Weather Map</a>.</p>
